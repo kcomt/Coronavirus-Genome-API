7ed57ebe6c813278af8e0d55a4ca3059a0a3eeb8
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2020-07-07 19:07:26 -0500

    created project

---
+++ src/main/java/com/BI/CoronavirusGenomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.BI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusGenomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusGenomeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BI/CoronavirusGenomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusGenomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
